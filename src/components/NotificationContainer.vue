e86acc621f1812c28c44535b43048277a3a93103
Sean Marzug-McCarthy <samdec11@gmail.com>
2023-06-05 14:36:48 -0400

    Finished Real World Vue 2 and Mastering Vuex

---
+++ src/components/NotificationContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="notification-container">
+    <NotificationBar
+      v-for="notification in notifications"
+      :notification="notification"
+      :key="notification.id"
+    />
+  </div>
+</template>
+
+<script>
+import NotificationBar from '@/components/NotificationBar.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    NotificationBar,
+  },
+  computed: mapState('notification', ['notifications']),
+}
+</script>
+
+<style scoped>
+.notification-container {
+  position: fixed;
+  bottom: 0;
+}
+</style>
