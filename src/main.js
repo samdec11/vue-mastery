5ed33898586bef0acd6bf4ef3af6ee6755174f94
Sean Marzug-McCarthy <samdec11@gmail.com>
2023-06-05 16:52:57 -0400

    Revert "Next-Level Vue 2 In-Component Route Guards"
    This reverts commit c555f7121818658b5f9e393458ed7affc2f479f8.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
-import 'nprogress/nprogress.css'
 
 const requireComponent = require.context(
   './components',
